8759cf4f77d614afe6d1c910e280c15c76c12cbc
XanderRoy <Xanendorf@gmail.com>
2018-12-17 20:22:48 -0600

    better placement  of globe

--- static/js/sidepanel.js
+++ static/js/sidepanel.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active')
         $('#content').toggleClass('active');
+
+        // this doesnt seem to work
+        // $('#globebox').attr('style',  ('left', 0))
     });
 
 });
