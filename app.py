d267f7b696a78877c55af61a6a9ca3145a514ead
XanderRoy <Xanendorf@gmail.com>
2019-01-10 16:35:33 -0600

    mongodb conversion underway

--- app.py
+++ app.py
@@ -17,38 +17,8 @@ from bson.json_util import dumps
 from config import  MONGOPASS
 
 # Database Setup
-engine = create_engine("sqlite:///db/Migrationdb.sqlite?check_same_thread=False")
-
-# reflect existing database into new model
-Base = automap_base()
-
-# reflect tables
-Base.prepare(engine, reflect=True)
-
-# save reference to tables
-migration = Base.classes.migration
-migration_age_group = Base.classes.migration_age_group
-trafficking_age_group = Base.classes.trafficking_age_group
-population = Base.classes.population
-gdp = Base.classes.gdp
-
-# create session to query tables
-session = Session(engine)
-
-# create inspector to get column names
-#inspector = inspect(engine)
-# Collect the names of tables within the database
-# print(inspector.get_table_names())
-
-# Using the inspector to print the column names within the 'migration' table and its types
-#columns = inspector.get_columns('migration')
-# for column in columns:
-#    print(column["name"], column["type"])
-
 app = Flask(__name__)
-
-
-app.config['MONGO_URI'] = "mongodb://traffickAdmin:"+ MONGOPASS + "@traffickcluster-shard-00-00-xeuqd.mongodb.net:27017,traffickcluster-shard-00-01-xeuqd.mongodb.net:27017,traffickcluster-shard-00-02-xeuqd.mongodb.net:27017/TraffickDB?ssl=true&replicaSet=TraffickCluster-shard-0&authSource=admin&retryWrites=true"
+app.config['MONGO_URI'] = "mongodb://traffickAdmin:"+ MONGOPASS + "@traffickcluster-shard-00-00-xeuqd.mongodb.net:27017,traffickcluster-shard-00-01-xeuqd.mongodb.net:27017,traffickcluster-shard-00-02-xeuqd.mongodb.net:27017/Neurath?ssl=true&replicaSet=TraffickCluster-shard-0&authSource=admin&retryWrites=true"
 mongo = PyMongo(app)
 
 # create route to return possible gender options for select menu
@@ -58,10 +28,10 @@ mongo = PyMongo(app)
 def fetch_country():
     # get possible country name values
     country_list = []
-    response = session.query(distinct(migration_age_group.countries_of_destination)).order_by(migration_age_group.countries_of_destination).all()
+    response = mongo.db.Migration_Counts.distinct('countries_of_destination')
+    # response = session.query(distinct(migration_age_group.countries_of_destination)).order_by(migration_age_group.countries_of_destination).all()
     for country in response:
-        temp, = country
-        country_list.append(temp)
+        country_list.append(country)
 
     # return response object
     return jsonify(country_list)
@@ -74,7 +44,7 @@ def fetch_country():
 @app.route('/fetch_year/<year>')
 def fetch_year(year):
     geojson = {'type':'FeatureCollection', 'features':[]}
-    response = mongo.db.trafficking.find({'properties.Year': int(year)})
+    response = mongo.db.Trafficking_GeoData.find({'properties.Year': int(year)})
     for feature in response:
        geojson['features'].append(feature)
     return dumps(geojson)
