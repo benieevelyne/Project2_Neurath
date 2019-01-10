296ab2cd9f3fb281ff41652683f05448b3fa2a2c
benieevelyne <40615779+benieevelyne@users.noreply.github.com>
2019-01-10 11:26:58 -0600

    Fixed issues that prevented to see graph and add (this.value) to the html  for my graph

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,3 +1,8 @@
+//As soon as the html is render, it fires anything in document ready. It grabs whatever has id of countryselector (which happens to be a dropdown for the country). Then it sets the value to USA
+$(document).ready(function(){
+    $('#countrySelectStackedBar').val('United States of America');
+    $('#countrySelectStackedBar').trigger('change'); //This fires the change event - Changing the value in the dropdown list.
+});
 
 //create select dropdown lists
 var $countrySelectListStackedBar = document.getElementById("countrySelectStackedBar");
@@ -14,25 +19,40 @@ function countries (){
     })
     
 }
+
+
+
 countrieslist = countries();
 //select is typically for things that you haven't created yet.
 //var countrydropdown = d3.selectAll('#countrySelectStackedBar').data(countrieslist).enter().append('option').attr('value', d=> d).text(d=> d);
-
-function buildPlot() {
-    /* data route */
-    var selectbar = d3.select('#countrySelectStackedBar')
-    var country = selectbar.node().selectedOptions[0].value
-
+function optionChangedStackedBar(country) {
+    
+    //set url for flask route using select objects
     var url = `/api/data/${country}`; //placeholder syntax for javascript
 d3.json(url).then(function(response) {
 // d3.json(url).then(function(response))
-console.log(response);
+    console.log(response);
 
+    //get data from response object
+    // countriesToPlot = response.countries;
+    // youthData = response.youth_data;
+    // adultData = response.adult_data;
+    // elderData = response.elder_data;
     var data = response;
 
+    yearData = response.map( d => d.years)
+    youthData = response.map( d => d.TotalYouth)
+    adultData = response.map( d => d.TotalAdult)
+    elderData = response.map( d => d.TotalElder)
+    console.log(yearData)
+
+    
+    console.log(youthData)
+    console.log(adultData)
+    console.log(elderData)
      //set up data traces
      var youthTrace = {
-        x: countriesToPlot,
+        x: yearData,
         y: youthData,
         name: 'total youth',
         type: 'bar',
@@ -42,7 +62,7 @@ console.log(response);
     };
 
     var adultTrace = {
-        x: countriesToPlot,
+        x: yearData,
         y: adultData,
         name: 'total adult',
         type: 'bar',
@@ -52,7 +72,7 @@ console.log(response);
     };
 
     var elderTrace = {
-        x: countriesToPlot,
+        x: yearData,
         y: elderData,
         name: 'total elder',
         type: 'bar',
@@ -77,18 +97,31 @@ console.log(response);
         title: 'migration data per Country'
     };
 
-
+//Create plot
     Plotly.newPlot("stackedBar", data, layout);
   });
 }
+// function buildPlot() {
 
 
-var selectbar = d3.select('#countrySelectStackedBar')
-selectbar.on('change',buildPlot)
+// /* data route */
+//     var selectbar = d3.select('#countrySelectStackedBar')
+//     console.log(selectbar.node().selectedOptions)
+//     var country = selectbar.node().selectedOptions[0].value
+//     country.value = `United States of America`
+//     console.log(country)
 
 
+// optionChangedStackedBar(country);
 
-//buildPlot();
+// }
+
+// var selectbar = d3.select('#countrySelectStackedBar')
+// selectbar.on('change',buildPlot) WHEN A CHANGE EVENT IS FIRED , DO THE PROPERTY....
+//          .property("selected", d=>{d == "United States of America"})
+
+
+// buildPlot("United States of America");
 
 
 
