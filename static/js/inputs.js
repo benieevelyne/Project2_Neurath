77555104bf20eada0b9a90ad541019be9131d564
XanderRoy <Xanendorf@gmail.com>
2019-01-16 01:40:38 -0600

    js linting https://www.jslint.com/

--- static/js/inputs.js
+++ static/js/inputs.js
@@ -1,24 +1,25 @@
 var slider = document.getElementById("timeSlider");
 var label = d3.select("#timesliderlabel");
-var goTime = d3.select('#goTime');
+var goTime = d3.select("#goTime");
 
-var isplaying = false
-var IntervalId = null
+var isplaying = false;
+var IntervalId = null;
 
 slider.oninput = function() {
-    label.html(`<h1><b>${slider.value}</b></h1>`)
-    if (isplaying == true) {stop(IntervalId)};
+    label.html(`<h1><b>${slider.value}</b></h1>`);
+    if (isplaying == true) {
+        stop(IntervalId);
+    }
     buildMap(slider.value);
 };
 
 
 go = function() {
-    console.log(slider.value)
     if (slider.value == 2018) {
         slider.value = 2002;
     } else {
         slider.value ++;
-    };
+    }
     label.html(`<h1><b>${slider.value}</b></h1>`);
     buildMap(slider.value);
 };
@@ -26,30 +27,30 @@ go = function() {
 stop = function(IntervalId) {
     isplaying = false;
     clearInterval(IntervalId);
-    goTime.html("<i class='fas fa-play-circle'></i><span>Play Animation</span>");
-}
+    goTime.html(
+        "<i class='fas fa-play-circle'></i><span>Play Animation</span>");
+};
 
-goTime.on('click', function(){
+goTime.on("click", function(){
 
     if (isplaying == false){
         isplaying = true;
-        goTime.html("<i class='fas fa-pause-circle'></i><span>Pause Animation</span>");
-       IntervalId = setInterval(go, 5000);   
-         
+        goTime.html(
+            "<i class='fas fa-pause-circle'></i><span>Pause Animation</span>");
+       IntervalId = setInterval(go, 5000);
         } else {
             stop(IntervalId);
-        };
+        }
 });
 
 noshare = d3.select("#noShare");
 
-noshare.on('click', function(){
+noshare.on("click", function(){
     console.log(viewer.dataSources.length);
     if (viewer.dataSources.length > 1) {
         viewer.dataSources.removeAll();
         buildMap(slider.value);
     } else {
-    
     var url = `/nodata`;
     d3.json(url).then(function(response) {
         var promise2 = Cesium.GeoJsonDataSource.load(response);
@@ -59,7 +60,7 @@ noshare.on('click', function(){
 
 
             for (var i = 0; i < entities.length; i++) {
-                var entity = entities[i];      
+                var entity = entities[i];
                 entity.polygon.material = Cesium.Color.black;
                 entity.polygon.extrudedHeight == 100;
                 entity.polygon.outline = false;
